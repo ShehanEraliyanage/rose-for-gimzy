32ee549a89230c02dd2aadbfe5933fd4e4e53ccb
Shehan Liyanage <76708200+ShehanEraliyanage@users.noreply.github.com>
2025-02-23 12:54:44 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Import HashRouter
 import App from "./App";
 
+// Create a root for React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Wrap the App component with HashRouter
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
